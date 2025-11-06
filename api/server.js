351c714f8c44008accfad7d49cfebc8266ba2685
mdparvej <m.parvej68@gmail.com>
2025-11-06 21:45:13 +0600

    second

--- api/server.js
+++ api/server.js
@@ -1,6 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('../index');
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+
 module.exports.handler = serverless(app);
